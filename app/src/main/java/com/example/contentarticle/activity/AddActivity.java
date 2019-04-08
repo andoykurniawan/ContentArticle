5ac104550b3f6b614015302beac83448e7422530
andy78kurniawan@gmail.com <lampard78>
2019-04-08 14:10:22 +0700

    Create DatePicker

--- app/src/main/java/com/example/contentarticle/activity/AddActivity.java
+++ app/src/main/java/com/example/contentarticle/activity/AddActivity.java
@@ -1,17 +1,31 @@
 package com.example.contentarticle.activity;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.RadioButton;
 
 import com.example.contentarticle.R;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 
 public class AddActivity extends AppCompatActivity {
 
     ImageView btn_back;
+    EditText txt_judul, txt_content, txt_tgl, txt_phone;
+    RadioButton radio_male, radio_female;
+    Button btn_save;
+
+    final Calendar myCalender = Calendar.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +33,13 @@ public class AddActivity extends AppCompatActivity {
         setContentView(R.layout.activity_add);
 
         btn_back = (ImageView) findViewById(R.id.back);
+        txt_judul = (EditText) findViewById(R.id.judul);
+        txt_content = (EditText) findViewById(R.id.content1);
+        txt_tgl = (EditText) findViewById(R.id.tanggal);
+        txt_phone = (EditText) findViewById(R.id.phone);
+        radio_male = (RadioButton) findViewById(R.id.male);
+        radio_female = (RadioButton) findViewById(R.id.female);
+        btn_save = (Button) findViewById(R.id.save);
 
         btn_back.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -26,8 +47,32 @@ public class AddActivity extends AppCompatActivity {
                 startActivity(new Intent(AddActivity.this, HomeActivity.class));
             }
         });
+
+        txt_tgl.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                new DatePickerDialog(AddActivity.this, date, myCalender.get(Calendar.YEAR),
+                        myCalender.get(Calendar.MONTH), myCalender.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
     }
 
+    DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
+        @Override
+        public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+            myCalender.set(Calendar.YEAR, year);
+            myCalender.set(Calendar.MONTH, monthOfYear);
+            myCalender.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+            updateLabel();
+        }
+    };
 
+
+    private void updateLabel() {
+        String myFormat = "MM/dd/yy";
+        SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
+        txt_tgl.setText(sdf.format(myCalender.getTime()));
+    }
 
 }
